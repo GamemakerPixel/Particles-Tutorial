1a4812814dc39ef8997356ed549b699eb4e528f5
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2020-08-15 14:03:06 -0400

    Create Player.gd

---
+++ Player.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+const SPEED = 500
+
+func _physics_process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+	else:
+		velocity.x = 0
+	if Input.is_action_pressed("ui_down"):
+		velocity.y = SPEED
+	elif Input.is_action_pressed("ui_up"):
+		velocity.y = -SPEED
+	else:
+		velocity.y = 0
+	velocity = move_and_slide(velocity)
